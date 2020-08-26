0f394ed6733e6aac589529852dd79a5ff9777218
Bruno <bruno.navarreteon@gmail.com>
2020-08-26 08:42:00 -0500

    propTypes

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,14 +1,26 @@
-import React from 'react'	
+import React from 'react';
+import PropTypes from 'prop-types';
 import classes from './Person.css'
+import withClass from '../../../hoc/withClass'
 
 const person = (props) => {
     return (
-        <div className={ classes.Person }>
+        <div>
     		<p onClick={ props.click }>I'm { props.name } and I am { props.age } years old</p>
     		<p>{ props.children }</p>
-    		<input type="text" onChange={ props.changed } value={ props.name } />
+    		<input 
+    		 type="text" 
+    		 onChange={ props.changed } 
+    		 value={ props.name } />
 		</div>
     )
 }
 
-export default person
+person.propTypes = {
+	click: PropTypes.func,
+	name: PropTypes.string,
+	age: PropTypes.number,
+	changed: PropTypes.func
+}
+
+export default withClass(person, classes.Person)
