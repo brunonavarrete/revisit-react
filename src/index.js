29d6c3364a8444c77b0403a6a9bfc76b17e5faaa
Bruno <bruno.navarreteon@gmail.com>
2020-08-30 07:56:02 -0500

    context type and usecontext

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './containers/App';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App appTitle="Person Management" />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
-
-
